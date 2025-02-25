76791866e6c21d8f1698279f7866f73a37cea592
AuroreTnr <tournieraurore@orange.fr>
2025-02-25 09:52:17 +0100

    ajout des constraint et de l unique Entity sur l email

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -11,6 +11,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterUserType extends AbstractType
 {
@@ -21,28 +23,45 @@ class RegisterUserType extends AbstractType
                 'label' => 'Entrez votre nom',
                 'attr' => [
                     'placeholder' => 'votre nom'
-                ]
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ])
             ])
             ->add('prenom', TextType::class, [
                 'label' => 'Entrez votre prénom',
                 'attr' => [
                     'placeholder' => 'votre prénom'
-                ]
+                ],
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 30
+                ])
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Entrez votre email',
                 'attr' => [
                     'placeholder' => 'votre email'
-                ]
+                ],
+                'constraints' => new Email([
+                    'message' => 'Cette email {{value}} n\' est pas valide'
+                ])
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options'  => [
-                    'label' => 'Entrez votre mot de passe',
-                    'hash_property_path' => 'password'
+                    'label' => 'Choisissez votre mot de passe',
+                    'hash_property_path' => 'password',
+                    'attr' => [
+                        'placeholder' => 'Entrez votre mot de passe'
+                    ]
                 ],
                 'second_options' => [
-                    'label' => 'Confirmez votre mot de passe'
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmez votre mot de passe'
+                    ]
                 ],
                 'mapped' => false,
             ])            
