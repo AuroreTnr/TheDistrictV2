f496e316c19cb6fbe72c66e454560579a6255685
AuroreTnr <tournieraurore@orange.fr>
2025-03-12 10:06:07 +0100

    mise en place d' un service pagination

--- src/Classe/Pagination.php
+++ src/Classe/Pagination.php
@@ -2,15 +2,41 @@
 
 namespace App\Classe;
 
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
 
 class Pagination
 {
-    public $doctrine;
 
-    public function __construct(ManagerRegistry $doctrine)
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+
+
+
+    public function setPagination($classRepository, $nbre_objet_par_page, $page)
+    {
+
+        $repository = $this->entityManager->getRepository($classRepository);
+
+        $nbre_total_objet = $repository->count();
+
+        $nbre_de_page = ceil($nbre_total_objet / $nbre_objet_par_page);
         
+        $page = max(1, min($page, $nbre_de_page));
+
+        $objets = $repository->findBy([],[], $nbre_objet_par_page, ($page - 1) * $nbre_objet_par_page);
+
+
+        return [
+            'objets' => $objets,
+            'isPaginated' => true,
+            'nbre_de_page' => $nbre_de_page,
+            'page' => $page
+        ];
+
     }
 
 
--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Classe\Pagination;
+use App\Entity\Categorie;
 use App\Entity\Plat;
 use App\Repository\CategorieRepository;
 use App\Repository\PlatRepository;
@@ -14,6 +16,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CatalogueController extends AbstractController
 {
 
+    private $pagination;
+
+    public function __construct(Pagination $pagination)
+    {
+        $this->pagination = $pagination;
+    }
+
+
+
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
@@ -22,40 +34,28 @@ final class CatalogueController extends AbstractController
 
 
     #[Route('/plats/{page?1}', name: 'app_plats')]
-    public function showPlats(Request $request, PlatRepository $platRepository, $page): Response
+    public function showPlats(Request $request, $page): Response
     {
-        $nbre_total_plat = $platRepository->count();
-
-        $nbre_de_plat_par_page = 6;
-
-        $plats = $platRepository->findBy([],[], $nbre_de_plat_par_page, ($page - 1) * $nbre_de_plat_par_page);
-
-        $nbre_de_page = ceil($nbre_total_plat / $nbre_de_plat_par_page);
+        $result = $this->pagination->setPagination(Plat::class, 6, $page);
 
         return $this->render('catalogue/plats.html.twig', [
-            'plats' => $plats,
-            'isPaginated' => true,
-            'nbre_de_page' => $nbre_de_page,
-            'page' => $page
+            'plats' => $result['objets'],
+            'isPaginated' => $result['isPaginated'],
+            'nbre_de_page' => $result['nbre_de_page'],
+            'page' => $result['page']
         ]);
     }
 
     #[Route('/categorie/{page?1}', name: 'app_categorie')]
-    public function showCategorie(Request $request, CategorieRepository $categorieRepository, $page): Response
+    public function showCategorie(Request $request, $page): Response
     {
-        $nbre_total_plat = $categorieRepository->count();
-
-        $nbre_de_plat_par_page = 4;
-
-        $categories = $categorieRepository->findBy([],[], $nbre_de_plat_par_page, ($page - 1) * $nbre_de_plat_par_page);
-
-        $nbre_de_page = ceil($nbre_total_plat / $nbre_de_plat_par_page);
+        $result = $this->pagination->setPagination(Categorie::class, 4, $page);
 
         return $this->render('catalogue/categorie.html.twig', [
-            'categories' => $categories,
-            'isPaginated' => true,
-            'nbre_de_page' => $nbre_de_page,
-            'page' => $page
+            'categories' => $result['objets'],
+            'isPaginated' => $result['isPaginated'],
+            'nbre_de_page' => $result['nbre_de_page'],
+            'page' => $result['page']
         ]);
     }
 }
