7e9978078be1cf43cd6dd54d46acd99937cc1689
AuroreTnr <tournieraurore@orange.fr>
2025-03-25 14:32:35 +0100

    mise en place de la customisation de la facture pdf avec dompdf

--- src/Controller/FactureController.php
+++ src/Controller/FactureController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use App\Repository\CommandeRepository;
+use Dompdf\Dompdf;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class FactureController extends AbstractController
 {
-    #[Route('/facture', name: 'app_facture')]
-    public function index(): Response
+    /**
+     * 
+     * impression facture pdf pour un utilisateur connecté
+     * vérification de la commande pour un utilisateur donné
+     * 
+     */
+    #[Route('/compte/facture/impression/{id_commande}', name: 'app_facture_client')]
+    public function index(CommandeRepository $commandeRepository, $id_commande): Response
     {
-        return $this->render('facture/index.html.twig', [
-            'controller_name' => 'FactureController',
+        $commande = $commandeRepository->findOneById($id_commande);
+        
+        if (!$commande) {
+            return $this->redirectToRoute('app_account');
+        }
+
+        if ($commande->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('app_account');
+        }
+
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+
+        $html = $this->renderView('facture/index.html.twig', [
+            'commande' => $commande
         ]);
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream('facture.pdf', [
+            'Attachment' => false
+        ]);
+
+        exit();
+
+        // return $this->render('facture/index.html.twig', [
+        //     'controller_name' => 'FactureController',
+        // ]);
     }
 }
