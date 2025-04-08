7a171aaac305e821cede4b6aa651b79be6921147
AuroreTnr <tournieraurore@orange.fr>
2025-04-08 12:02:14 +0200

    essai de la mise en plase d un api sur les plats

--- src/Entity/Plat.php
+++ src/Entity/Plat.php
@@ -3,45 +3,59 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\PlatRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PlatRepository::class)]
-#[ApiResource()]
+#[ApiResource(
+    operations: [
+            new Get(normalizationContext: ['groups' => 'plat:read']),
+            new GetCollection(normalizationContext: ['groups' => 'plat:read'])
+    ]
+)]
 class Plat
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?string $libelle = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?string $description = null;
 
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?float $prix = null;
 
     #[ORM\Column]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?float $tva = null;
 
     #[ORM\Column(length: 225)]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?string $image = null;
 
     #[ORM\Column]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?bool $active = null;
 
     #[ORM\ManyToOne(inversedBy: 'plats')]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?Categorie $categorie = null;
 
 
     #[ORM\Column(length: 255)]
+    #[Groups(['plat:read' ,'comment:list', 'comment:item'])]
     private ?string $slug = null;
 
 
